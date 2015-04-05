2398342fc0a2859e2584fd718c166d67028d6787
autarch princeps <autarch@outlook.com>
2015-04-06 01:36:25 +0200

    -

--- Firefox/start.sh
+++ Firefox/start.sh
@@ -1,1 +1,1 @@
-sudo docker run -ti --rm -e DISPLAY=:0 -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/autarch/Downloads:/home/autarch/Downloads firefox
+sudo docker run -ti --rm -e DISPLAY=:0 -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/autarch/Downloads:/home/autarch/Downloads autarchprinceps/firefox
